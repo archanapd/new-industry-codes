190c6cb987872e0d36e8218e6875119dc9293fed
Divya P <panackal.divya@gmail.com>
2022-07-28 21:21:36 +0530

    Home page

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Container, Box, AppBar, Typography, Toolbar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import './NavBar.scss';
-
-import { Container, Box } from '@mui/material';
 
 export default function NavBar(props: any) {
   return (
-    <div className="App">
-      <Container maxWidth="md">
-        <Box sx={{ bgcolor: 'white', height: '1000px' }}>NavBar</Box>
-      </Container>
-    </div>
+    <Box>
+      <AppBar className="header p-4" style={{ backgroundColor: '#252525', boxShadow: 'none' }}>
+        <div className='container'>
+          <Toolbar className='p-0'>
+            <Typography
+              variant="h6"
+              noWrap
+              component="h1"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              className="logo"
+              style={{ fontSize: '2rem', fontFamily:'Poppins', fontWeight: 'bold'}}
+            >
+              Industry codes
+            </Typography>
+            <IconButton size="large" aria-label="search" color="inherit">
+              <SearchIcon style={{ fontSize: '3rem'}}/>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon style={{ fontSize: '3rem'}}/>
+            </IconButton>
+          </Toolbar>
+        </div>
+      </AppBar>
+    </Box>
   );
 }
